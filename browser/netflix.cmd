1bdd7770d2e04539eac65c4c9f1785819ba4c84e
Fabian Seitz <seitzf1@yahoo.de>
2018-01-29 20:37:59 +0100

    Add files via upload

---
+++ browser/netflix.cmd
@@ -0,0 +1,4 @@
+E:\Push2Run\nircmd\nircmd setdefaultsounddevice "Lautsprecher" 1
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://netflix.com
+timeout /T 3
+start E:\WebExtractor\netflix.vbs
---
+++ browser/soundcloud.cmd
@@ -0,0 +1,4 @@
+E:\Push2Run\nircmd\nircmd setdefaultsounddevice "Lautsprecher" 1
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://soundcloud.com
+timeout /T 3
+start E:\Push2Run\browser\soundcloud.vbs
