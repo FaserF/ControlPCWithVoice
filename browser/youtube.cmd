8d85b546044b973473ff5679886ee83db023540a
Fabian Seitz <seitzf1@yahoo.de>
2018-01-31 14:53:09 +0100

    Updated youtube

--- browser/youtube.cmd
+++ browser/youtube.cmd
@@ -1,6 +1,9 @@
+@echo off
 set search=%1
+
+echo Starting YouTube %search%
 
 E:\Push2Run\nircmd\nircmd setdefaultsounddevice "Lautsprecher" 1
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/results?search_query=%search%"
-timeout /T 3
+timeout /T 3 > :NULL
 start E:\Push2Run\browser\youtube-search.vbs
