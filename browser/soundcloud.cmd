4b99c96705286ff47b0326f21f14d1a596303fcf
Fabian Seitz <seitzf1@yahoo.de>
2018-02-15 20:03:10 +0100

    Small improvements

--- browser/soundcloud.cmd
+++ browser/soundcloud.cmd
@@ -1,3 +1,5 @@
+@echo off
+echo "Starting Soundcloud..."
 E:\Push2Run\nircmd\nircmd setdefaultsounddevice "Lautsprecher" 1
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://soundcloud.com
 timeout /T 3
--- browser/youtube.cmd
+++ browser/youtube.cmd
@@ -1,9 +1,11 @@
 @echo off
 set search=%1
+set search2=%2
+set search3=%3
 
 echo Starting YouTube %search%
 
 E:\Push2Run\nircmd\nircmd setdefaultsounddevice "Lautsprecher" 1
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/results?search_query=%search%"
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/results?search_query=%search%+%search2%+%search3%"
 timeout /T 3 > :NULL
 start E:\Push2Run\browser\youtube-search.vbs
